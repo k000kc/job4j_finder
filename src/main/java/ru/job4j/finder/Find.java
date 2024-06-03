89d9674f4b26e4bfb8598ad1631ef3e589f1079c
Andrey <ham2188@mail.ru>
2024-06-03 20:56:37 +0300

    add method checkTupe

--- src/main/java/ru/job4j/finder/Find.java
+++ src/main/java/ru/job4j/finder/Find.java
@@ -8,6 +8,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 public class Find {
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
@@ -24,10 +25,37 @@ public class Find {
             }
         }
     }
+
+    private Predicate<Path> checkType(ArgsName args) {
+        Predicate<Path> predicate = null;
+        String type = args.get("t");
+        String name = args.get("n");
+        if (type.equals("name")) {
+            predicate = path -> path.getFileName().toFile().toString()
+                    .equals(name);
+        }
+        if (type.equals("mask")) {
+            predicate = path -> path.getFileName().toFile().toString()
+                    .matches(maskToRegex(name));
+        }
+        if (type.equals("regex")) {
+            predicate = path -> Pattern.compile(name)
+                    .matcher(path.getFileName().toFile().toString())
+                    .matches();
+        }
+        return predicate;
+    }
+
+    private String maskToRegex(String mask) {
+        return mask.replace("*", "\\S*")
+                .replace("?", "\\S{1}")
+                .replace(".", "\\.");
+    }
+
     public static void main(String[] args) throws IOException {
+        Find find = new Find();
         ArgsName argsName = ArgsName.of(args);
-        List<Path> paths = search(Paths.get(argsName.get("d")),
-                path -> path.toFile().getName().equals(argsName.get("n")));
-        new Find().writeToFile(paths, new File(argsName.get("o")));
+        List<Path> paths = search(Paths.get(argsName.get("d")), find.checkType(argsName));
+        find.writeToFile(paths, new File(argsName.get("o")));
     }
 }
